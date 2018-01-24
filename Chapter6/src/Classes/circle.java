feab908cecc2769ce06e465781cf4964b00e46a0
Jacob <jacobedwardhaig@gmail.com>
2018-01-24 09:24:31 -0500

    Update circle.java

--- Chapter6/src/Classes/circle.java
+++ Chapter6/src/Classes/circle.java
@@ -8,19 +8,18 @@ package Classes;
 
 public class circle {
 	circle(){
-		radius = 0;
+		radius = 1;
 	}
 	circle(double r){
 		radius = r;
 	}
 	
 	private double radius;
-	private double PI = Math.PI;
 	
 	public void 	setRadius(double r)	{	radius = r;			}
-	public double 	getRadius()			{	return radius;		}
+	public double 	getRadius()		{	return radius;		}
 	public double 	getDiameter()		{	return 2 * radius;	}
-	public double 	getPI() 			{	return PI;			}
-	public double 	getArea()			{	return Math.pow(radius, 2) * Math.PI;	}
+	public double 	getPI() 		{	return Math.PI;			}
+	public double 	getArea()		{	return Math.pow(radius, 2) * Math.PI;	}
 	public double   getCircumference()	{	return 2 * Math.PI * radius;	}
 }
