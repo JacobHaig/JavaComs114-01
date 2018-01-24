05f1cca123e9828da4af81831a78f4fc64dd93d0
Jacob <jacobedwardhaig@gmail.com>
2018-01-24 09:22:37 -0500

    Update Temp.java

--- Chapter6/src/problemNine/Temp.java
+++ Chapter6/src/problemNine/Temp.java
@@ -14,7 +14,7 @@ public class Temp {
 	}
 	
 	public void setFahrenheit(double temp){	ftemp = temp;	}
-	public double getFahrenheit()	{	return ftemp;	}
+	public double getFahrenheit()		{	return ftemp;	}
 	public double getCelsius()		{	return ((5.0 / 9.0) * (ftemp - 32.0));	}
 	public double getKelvin()		{	return getCelsius() + 273.0;	}
 	
